b8cfe756c51304d6dba6facb78c5ec4ffa31d883
Chris <c.r.zambito@gmail.com>
2023-03-07 00:00:47 -0500

    feature 16 complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from tasks.views import create_task
+from tasks.views import create_task, show_my_tasks
 
 
 urlpatterns = [
+    path('mine/', show_my_tasks, name='show_my_tasks'),
     path('projects/', include('projects.urls')),
     path('create/', create_task, name='create_task'),
 ]
