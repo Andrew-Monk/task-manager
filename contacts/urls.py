be34f5c61c12447c8dadec8ec395b7e78898a29f
Danielle Aharonov <daharonov27@gmail.com>
2023-03-15 17:51:20 -0400

    Danielle's 2nd commit

---
+++ contacts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from contacts.views import list_contacts
+
+
+urlpatterns = [
+    path("", list_contacts, name="list_contacts"),
+]
--- tracker/urls.py
+++ tracker/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path("", redirect_home, name="home"),
     path("projects/", include("projects.urls")),
     path("admin/", admin.site.urls),
+    path("contacts/", include("contacts.urls")),
 ]
