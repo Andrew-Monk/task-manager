b1d97f3c19008276d1fa87a5d2b827746eef0314
Chris <c.r.zambito@gmail.com>
2023-03-15 23:13:48 -0400

    changed a bunch of stuff added a few game stuff

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from tasks.views import create_task, show_my_tasks
+from tasks.views import create_task, show_my_tasks, toggle_task_completion
 
 
 urlpatterns = [
     path("mine/", show_my_tasks, name="show_my_tasks"),
     path("projects/", include("projects.urls")),
     path("create/", create_task, name="create_task"),
+    path('toggle_task_completion/<int:task_id>/', toggle_task_completion, name='toggle_task_completion'),
 ]
